e6e4069ae48bfe51f5f36d2e9b6cd9becc87a0fa
Valery Piashchynski <piashchynski.valery@gmail.com>
2024-06-04 22:45:45 +0200

    chore: update linters
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- tcplisten_bsd.go
+++ tcplisten_bsd.go
@@ -8,12 +8,12 @@ import (
 
 const soReusePort = syscall.SO_REUSEPORT
 
-func enableDeferAccept(fd int) error {
+func enableDeferAccept(int) error {
 	// TODO: implement SO_ACCEPTFILTER:dataready here
 	return nil
 }
 
-func enableFastOpen(fd int) error {
+func enableFastOpen(int) error {
 	// TODO: implement TCP_FASTOPEN when it will be ready
 	return nil
 }
